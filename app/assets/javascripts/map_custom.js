ec64e82f567cee4c0f87bef9ae890599df196839
Thuong Ho <thuong.t.ho@gmail.com>
2014-05-15 12:42:38 -0700

    tidy up show page

--- app/assets/javascripts/map_custom.js
+++ app/assets/javascripts/map_custom.js
@@ -50,7 +50,7 @@ $(document).ready(function(){
   var panIsDisabled = true;
 
   $('.stop-pan').click(function(){
-    alert(panIsDisabled == true ? "Panning is turned off." : "Panning is turned on.");
+    // alert(panIsDisabled == true ? "Panning is turned off." : "Panning is turned on.");
     if (panIsDisabled == true){
       $('#cArea').panzoom("option", "disablePan", true);
       panIsDisabled = false;
@@ -69,7 +69,7 @@ $(document).ready(function(){
   $('#icons').on("click",".street1", function(){
     divCounter++;
     // alert(divCounter);
-    $("<div class='house' id='street" + divCounter + "' style='width:40px;height:40px;border:1px solid black;position:absolute;top:0;left:0;right:0;bottom:0;margin:auto;'><img src='/images/road1.svg' alt='street1' type='image/svg+xml'/></div>").appendTo('.canvas-area').draggable({ grid: [5,5]});
+    $("<div class='house' id='street" + divCounter + "' style='width:20px;height:20px;'><img src='/images/road1.svg' alt='street' type='image/svg+xml'/></div>").appendTo('.canvas-area').draggable({ grid: [2,2]});
     moveToFront();
   });
 
@@ -77,15 +77,79 @@ $(document).ready(function(){
   $('#icons').on("click",".street2", function(){
     divCounter++;
     // alert(divCounter);
-    $("<div class='house' id='street" + divCounter + "' style='width:40px;height:40px;border:1px solid black;'><img src='/images/road2.svg' alt='street2' type='image/svg+xml'/></div>").appendTo('.canvas-area').draggable({ grid: [1,1], stack: ".draggable"});
+    $("<div class='house' id='street" + divCounter + "' style='width:20px;height:20px;'><img src='/images/road2.svg' alt='street' type='image/svg+xml'/></div>").appendTo('.canvas-area').draggable({ grid: [2,2]});
     moveToFront();
   });
 
-  // road3
-  $('#icons').on("click",".street3", function(){
+  // road3-1
+  $('#icons').on("click",".street3-1", function(){
     divCounter++;
     // alert(divCounter);
-    $("<div class='house' id='street" + divCounter + "' style='width:40px;height:40px;border:1px solid black;'><img src='images/road_3way.svg' alt='street3' type='image/svg+xml'/></div>").appendTo('.canvas-area').draggable({ grid: [5,5]});
+    $("<div class='house' id='street" + divCounter + "' style='width:20px;height:20px;'><img src='/images/road_3way_1.svg' alt='street' type='image/svg+xml'/></div>").appendTo('.canvas-area').draggable({ grid: [2,2]});
+    moveToFront();
+  });
+
+  // road3-2
+  $('#icons').on("click",".street3-2", function(){
+    divCounter++;
+    // alert(divCounter);
+    $("<div class='house' id='street" + divCounter + "' style='width:20px;height:20px;'><img src='/images/road_3way_2.svg' alt='street' type='image/svg+xml'/></div>").appendTo('.canvas-area').draggable({ grid: [2,2]});
+    moveToFront();
+  });
+
+  // road3-3
+  $('#icons').on("click",".street3-3", function(){
+    divCounter++;
+    // alert(divCounter);
+    $("<div class='house' id='street" + divCounter + "' style='width:20px;height:20px;'><img src='/images/road_3way_3.svg' alt='street' type='image/svg+xml'/></div>").appendTo('.canvas-area').draggable({ grid: [2,2]});
+    moveToFront();
+  });
+
+  // road3-4
+  $('#icons').on("click",".street3-4", function(){
+    divCounter++;
+    // alert(divCounter);
+    $("<div class='house' id='street" + divCounter + "' style='width:20px;height:20px;'><img src='/images/road_3way_4.svg' alt='street' type='image/svg+xml'/></div>").appendTo('.canvas-area').draggable({ grid: [2,2]});
+    moveToFront();
+  });
+
+  // road4-1
+  $('#icons').on("click",".street4-1", function(){
+    divCounter++;
+    // alert(divCounter);
+    $("<div class='house' id='street" + divCounter + "' style='width:20px;height:20px;'><img src='/images/road_curve_1.svg' alt='street' type='image/svg+xml'/></div>").appendTo('.canvas-area').draggable({ grid: [2,2]});
+    moveToFront();
+  });
+
+  // road4-2
+  $('#icons').on("click",".street4-2", function(){
+    divCounter++;
+    // alert(divCounter);
+    $("<div class='house' id='street" + divCounter + "' style='width:20px;height:20px;'><img src='/images/road_curve_2.svg' alt='street' type='image/svg+xml'/></div>").appendTo('.canvas-area').draggable({ grid: [2,2]});
+    moveToFront();
+  });
+
+  // road4-3
+  $('#icons').on("click",".street4-3", function(){
+    divCounter++;
+    // alert(divCounter);
+    $("<div class='house' id='street" + divCounter + "' style='width:20px;height:20px;'><img src='/images/road_curve_3.svg' alt='street' type='image/svg+xml'/></div>").appendTo('.canvas-area').draggable({ grid: [2,2]});
+    moveToFront();
+  });
+
+  // road4-4
+  $('#icons').on("click",".street4-4", function(){
+    divCounter++;
+    // alert(divCounter);
+    $("<div class='house' id='street" + divCounter + "' style='width:20px;height:20px;'><img src='/images/road_curve_4.svg' alt='street' type='image/svg+xml'/></div>").appendTo('.canvas-area').draggable({ grid: [2,2]});
+    moveToFront();
+  });
+
+  // crossroad
+  $('#icons').on("click",".street5", function(){
+    divCounter++;
+    // alert(divCounter);
+    $("<div class='house' id='street" + divCounter + "' style='width:20px;height:20px;'><img src='/images/crossroad1.svg' alt='street' type='image/svg+xml'/></div>").appendTo('.canvas-area').draggable({ grid: [2,2]});
     moveToFront();
   });
 
@@ -94,52 +158,144 @@ $(document).ready(function(){
   $('#icons').on("click",".building1", function(){
     divCounter++;
     // alert(divCounter);
-    $("<div class='house apt' id='building" + divCounter + "' style='width:80px;height:80px;border:1px solid black;'><img src='images/building_apt.svg' alt='house' type='image/svg+xml'/></div>").appendTo('.canvas-area').draggable({stack: ".drag"});
-    // moveToFront();
+    $("<div class='house apt' id='building" + divCounter + "' style='width:80px;height:80px;'><img src='/images/building_apt.svg' alt='building' type='image/svg+xml'/></div>").appendTo('.canvas-area').draggable({grid: [2,2]});
+    moveToFront();
   });
 
   // office
   $('#icons').on("click",".building2", function(){
     divCounter++;
     // alert(divCounter);
-    $("<div class='house apt' id='building" + divCounter + "' style='width:80px;height:80px;border:1px solid black;'><img src='images/building_office.svg' alt='house' type='image/svg+xml' /></div>").appendTo('.canvas-area').draggable();
+    $("<div class='house apt' id='building" + divCounter + "' style='width:80px;height:80px;'><img src='/images/building_office.svg' alt='house' type='image/svg+xml' /></div>").appendTo('.canvas-area').draggable({grid: [2,2]});
+    moveToFront();
+  });
+
+  // factory
+  $('#icons').on("click",".building3", function(){
+    divCounter++;
+    // alert(divCounter);
+    $("<div class='house apt' id='building" + divCounter + "' style='width:80px;height:80px;'><img src='/images/factory_1.svg' alt='factory' type='image/svg+xml'/></div>").appendTo('.canvas-area').draggable({grid: [2,2]});
+    moveToFront();
+  });
+
+  // firestation
+  $('#icons').on("click",".building4", function(){
+    divCounter++;
+    // alert(divCounter);
+    $("<div class='house apt' id='building" + divCounter + "' style='width:80px;height:80px;'><img src='/images/fire_station.svg' alt='firestation' type='image/svg+xml'/></div>").appendTo('.canvas-area').draggable({grid: [2,2]});
+    moveToFront();
+  });
+
+  // hospital
+  $('#icons').on("click",".building5", function(){
+    divCounter++;
+    // alert(divCounter);
+    $("<div class='house apt' id='building" + divCounter + "' style='width:80px;height:80px;'><img src='/images/hospital.svg' alt='hospital' type='image/svg+xml'/></div>").appendTo('.canvas-area').draggable({grid: [2,2]});
+    moveToFront();
+  });
+
+  // house1
+  $('#icons').on("click",".building6", function(){
+    divCounter++;
+    // alert(divCounter);
+    $("<div class='house apt' id='building" + divCounter + "' style='width:40px;height:40px;'><img src='/images/house_1.svg' alt='house' type='image/svg+xml'/></div>").appendTo('.canvas-area').draggable({grid: [2,2]});
+    moveToFront();
+  });
+
+  // house2
+  $('#icons').on("click",".building7", function(){
+    divCounter++;
+    // alert(divCounter);
+    $("<div class='house apt' id='building" + divCounter + "' style='width:40px;height:40px;'><img src='/images/house_2.svg' alt='house' type='image/svg+xml'/></div>").appendTo('.canvas-area').draggable({grid: [2,2]});
+    moveToFront();
+  });
+
+  // house3
+  $('#icons').on("click",".building8", function(){
+    divCounter++;
+    // alert(divCounter);
+    $("<div class='house apt' id='building" + divCounter + "' style='width:40px;height:40px;'><img src='/images/house_3.svg' alt='house' type='image/svg+xml'/></div>").appendTo('.canvas-area').draggable({grid: [2,2]});
+    moveToFront();
+  });
+
+  // house4
+  $('#icons').on("click",".building9", function(){
+    divCounter++;
+    // alert(divCounter);
+    $("<div class='house apt' id='building" + divCounter + "' style='width:40px;height:40px;'><img src='/images/house_4.svg' alt='house' type='image/svg+xml'/></div>").appendTo('.canvas-area').draggable({grid: [2,2]});
+    moveToFront();
+  });
+
+  // police station
+  $('#icons').on("click",".building10", function(){
+    divCounter++;
+    // alert(divCounter);
+    $("<div class='house apt' id='building" + divCounter + "' style='width:80px;height:80px;'><img src='/images/police_station.svg' alt='police station' type='image/svg+xml'/></div>").appendTo('.canvas-area').draggable({grid: [2,2]});
     moveToFront();
   });
 
   // skyscraper1
-  $('#icons').on("click",".building3", function(){
+  $('#icons').on("click",".building11", function(){
     divCounter++;
     // alert(divCounter);
-    $("<div class='house sky' id='skyscraper" + divCounter + "' style='width:100px;height:100px;border:1px solid black;'><img src='images/skyscraper_1.svg' alt='house' type='image/svg+xml' /></div>").appendTo('.canvas-area').draggable();
+    $("<div class='house sky' id='skyscraper" + divCounter + "' style='width:100px;height:100px;'><img src='/images/skyscraper_1.svg' alt='building' type='image/svg+xml' /></div>").appendTo('.canvas-area').draggable({grid: [2,2]});
     moveToFront();
   });
 
   // skyscraper2
-  $('#icons').on("click",".building4", function(){
+  $('#icons').on("click",".building12", function(){
     divCounter++;
     // alert(divCounter);
-    $("<div class='house sky' id='skyscraper" + divCounter + "' style='width:100px;height:100px;border:1px solid black;'><img src='images/skyscraper_2.svg' alt='house' type='image/svg+xml' /></div>").appendTo('.canvas-area').draggable();
+    $("<div class='house sky' id='skyscraper" + divCounter + "' style='width:100px;height:100px;'><img src='/images/skyscraper_2.svg' alt='building' type='image/svg+xml' /></div>").appendTo('.canvas-area').draggable({grid: [2,2]});
     moveToFront();
   });
 
-  // transamerica
-  $('#icons').on("click",".building5", function(){
+  // skyscraper3
+  $('#icons').on("click",".building13", function(){
     divCounter++;
     // alert(divCounter);
-    $("<div class='house sky' id='building" + divCounter + "' style='width:100px;height:100px;border:1px solid black;'><img src='images/trans_america.svg' alt='house' type='image/svg+xml' /></div>").appendTo('.canvas-area').draggable();
+    $("<div class='house sky' id='skyscraper" + divCounter + "' style='width:100px;height:100px;'><img src='/images/skyscraper_3.svg' alt='building' type='image/svg+xml' /></div>").appendTo('.canvas-area').draggable({grid: [2,2]});
     moveToFront();
   });
+
+  // skyscraper4
+  $('#icons').on("click",".building14", function(){
+    divCounter++;
+    // alert(divCounter);
+    $("<div class='house sky' id='skyscraper" + divCounter + "' style='width:100px;height:100px;'><img src='/images/skyscraper_4.svg' alt='building' type='image/svg+xml' /></div>").appendTo('.canvas-area').draggable({grid: [2,2]});
+    moveToFront();
+  });
+
+
 
 
   // **** landmarks ****
-  // ballpark
-  $('#icons').on("click",".redLand", function(){
+  // transamerica
+  $('#icons').on("click",".landmark1", function(){
     divCounter++;
     // alert(divCounter);
-    $("<div class='house ball' id='building" + divCounter + "' style='width:120px;height:120px;border:1px solid black;'><img src='images/ballpark.svg' alt='ballpark' type='image/svg+xml' /></div>").appendTo('.canvas-area').draggable();
+    $("<div class='house sky' id='landmark" + divCounter + "' style='width:100px;height:100px;'><img src='/images/trans_america.svg' alt='transamerica' type='image/svg+xml' /></div>").appendTo('.canvas-area').draggable({grid: [2,2]});
+    moveToFront();
+  });
+
+  // ballpark
+  $('#icons').on("click",".landmark2", function(){
+    divCounter++;
+    // alert(divCounter);
+    $("<div class='house ball' id='building" + divCounter + "' style='width:120px;height:120px;'><img src='/images/ballpark.svg' alt='ballpark' type='image/svg+xml' /></div>").appendTo('.canvas-area').draggable({grid: [2,2]});
+    moveToFront();
+  });
+
+  // union square
+  $('#icons').on("click",".landmark3", function(){
+    divCounter++;
+    // alert(divCounter);
+    $("<div class='house ball' id='building" + divCounter + "' style='width:80px;height:80px;'><img src='/images/unionsquare.svg' alt='union sq' type='image/svg+xml' /></div>").appendTo('.canvas-area').draggable({grid: [2,2]});
     moveToFront();
   });
 });
+
+
 
 // jquery ui script for shopping cart
 $(function(){
